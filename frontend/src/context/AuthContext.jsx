9185d195d814327190ce9cc6273e6ef596527183
shvgpt-070208 <shvgpt070208@gmail.com>
2025-10-26 17:12:27 +0530

    server url

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createContext } from 'react'
 export const authDataContext= createContext()
 function AuthContext({children}) {
-    let serverUrl = "https://shopingai.vercel.app/"
+    let serverUrl = "https://shopingai-server.vercel.app"
 
     let value = {
        serverUrl
